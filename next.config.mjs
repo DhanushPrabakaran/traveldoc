589aaebe7f043945fe3b5e0f09e96539e0b5c666
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-03-16 19:54:08 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // pwa: {
-    //     manifest: './public/manifest.json'
-    //   }
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // pwa: {
+//     //     manifest: './public/manifest.json'
+//     //   }
+// };
+
+// export default nextConfig;
+import withPWA from "@ducanh2912/next-pwa";
+
+const nextConfig = withPWA({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
+});
 
 export default nextConfig;
