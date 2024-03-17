956ff6900be507bfab147fa04b8fa4d94da1e8ae
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-03-17 11:50:14 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
-import {GlobeDemo} from "@/components/GlobeDemo"
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import camper from "@/public/camper.png";
 export default function Home() {
   return (
-    <GlobeDemo/>
+    <div className=" bg-black w-screen h-screen">
+      <div className=" w-full bg-white h-20 rounded-br-full flex flex-row justify-between align-middle items-center ">
+        <Image src={camper} alt={""} width={140} className="" />
+        {/* <h1 className=" text-2xl font-serif font-bold uppercase">Wonder Wisper</h1> */}
+        <div
+        className=" mr-8">
+        <Button variant="ghost" className="m-2 bg-black text-white rounded-bl-full" size="lg">Button</Button>
+        <Button variant="ghost" className="m-2 bg-black text-white rounded-br-full" size="lg">Button</Button>
+        </div>
+        
+      </div>
+
+      <h1 className=" text-white font-serif text-5xl lg:text-7xl  ">
+        Helo hai
+      </h1>
+    </div>
   );
 }
