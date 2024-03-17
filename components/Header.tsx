597305763d7b2e848e8e7bd6d2d7dd69adc5143c
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-03-17 22:22:29 +0530

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,21 @@
-
-import Image from "next/image"
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div
-    className=" bg-black h-20 flex flex-row justify-around">
-        <h1
-        className="  text-4xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text inline-block"
-        >Wonder Wisper</h1>        
+    <div className="bg-lime-200 h-16 flex flex-row justify-between items-center rounded-full">
+      <h1 className="text-emerald-600 text-3xl max-md:text-2xl ml-3">
+        Wonder Wisper
+      </h1>
+      <div className=" flex flex-row mr-1">
+        {/* <button className="bg-white w-full h-full mx-1 p-3 px-6 rounded-3xl text-nowrap">
+          Sign up
+        </button> */}
+        <button className="bg-white w-full h-full mx-1 p-3 px-6 rounded-3xl text-nowrap text-emerald-600">
+          Sign-up / Login
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
