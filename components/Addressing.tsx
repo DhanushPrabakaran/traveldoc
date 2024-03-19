4a90954db1bad7419a67dc440b708acf79e1af7a
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-03-20 00:31:59 +0530

    update

--- components/Addressing.tsx
+++ components/Addressing.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import Image from "next/image";
+import Button from "./ui/Button";
+import Sea from "@/public/Sea.jpg";
+import bikingicon from "@/public/icon/bikingicon.svg";
 const Addressing = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="flex flex-col justify-center align-middle items-center text-center lg:w-1/2">
+        <h1 className=" text-2xl lg:text-4xl max-md:text-3xl font-bold">
+          Where are you now ?
+        </h1>
+        <div className=" bg-white bg-opacity-20 m-1 p-2 flex flex-wrap justify-center align-middle gap-1 ">
+          <Image
+            src={bikingicon}
+            alt={""}
+            className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"
+          />
+          <Image
+            src={bikingicon}
+            alt={""}
+            className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"
+          />
+          <Image
+            src={bikingicon}
+            alt={""}
+            className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"
+          />
+
+          <Image
+            src={bikingicon}
+            alt={""}
+            className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"
+          />
+        </div>
+      </div>
   )
 }
 
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import Button from "./ui/Button";
 import Sea from "@/public/Sea.jpg";
-import bikingicon from "@/public/icon/bikingicon.svg"
+import Addressing from "./Addressing";
+
 const LandingPage = () => {
-  
-
-
   return (
     <div className="flex flex-col lg:flex-row w-full max-md:h-full h-screen  bg-lime-200">
       <div className="bg-black rounded-xl h-full lg:w-1/2 m-2 lg:m-0 lg:rounded-l-none max-md:mx-2 lg:rounded-r-3xl relative flex flex-col justify-center align-middle items-center">
@@ -22,36 +20,7 @@ const LandingPage = () => {
           <Button text={"Get Started"}></Button>
         </div>
       </div>
-
-      <div className="flex flex-col justify-center align-middle items-center text-center lg:w-1/2">
-        <h1
-        className=" text-2xl lg:text-4xl max-md:text-3xl font-bold">Where are you now ?</h1>
-        <div
-        className=" bg-white bg-opacity-20 m-1 p-2 flex flex-wrap justify-center align-middle gap-1 ">
-        <Image 
-        src={bikingicon} 
-        alt={""}
-        
-        className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"/>
-        <Image 
-        src={bikingicon} 
-        alt={""}
-        
-        className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"  />
-        <Image 
-        src={bikingicon} 
-        alt={""}
-        
-        className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"  />
-        
-        <Image 
-        src={bikingicon} 
-        alt={""}
-        
-        className="bg-white w-14 h-14 hover:bg-green-800 lg:h-16 lg:w-16  rounded-xl"  />
-        </div>
-        
-      </div>
+      <Addressing/>
     </div>
   );
 };
