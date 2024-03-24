03ec5ea4e856c612de0385ead77c9ed1dfca6ec8
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-03-24 15:39:07 +0530

    update

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -30,9 +30,9 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className="bg-gray-950 w-full max-w-xl flex py-2 rounded-xl flex-wrap justify-center items-center align-middle border border-gray-800 my-4">
-        <div className=" flex flex-row justify-center items-center align-middle flex-grow">
-          <div className=" text-secondary">
+      <div className="bg-gray-950 flex-row max-w-6xl flex p-2 rounded-xl justify-evenly items-center align-middle border border-gray-800 my-4 flex-wrap">
+        <div className=" flex flex-row m-2 justify-evenly  items-center align-middle ">
+          
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -46,7 +46,7 @@ const LandingPage = () => {
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               ></path>
             </svg>
-          </div>
+          
           <div>
             <div className=" font-bold text-lg">Downloads</div>
             <div className=" lg:text-4xl max-sm:text-3xl text-lg font-extrabold font-sans">
@@ -56,7 +56,7 @@ const LandingPage = () => {
           </div>
         </div>
 
-        <div className=" flex flex-row justify-center items-center align-middle flex-grow">
+        <div className=" flex flex-row m-2 justify-evenly  items-center align-middle ">
           <div>
             <div className=" font-bold text-lg">New Users</div>
             <div className=" lg:text-4xl max-sm:text-3xl text-lg font-extrabold font-sans">
@@ -64,7 +64,7 @@ const LandingPage = () => {
             </div>
             <div className="stat-desc">↗︎ 400 (22%)</div>
           </div>
-          <div className=" text-secondary">
+          
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -78,10 +78,10 @@ const LandingPage = () => {
                 d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
               ></path>
             </svg>
-          </div>
+          
         </div>
 
-        <div className=" flex flex-row justify-center items-center align-middle flex-grow">
+        <div className=" flex flex-row m-2 justify-evenly  items-center align-middle ">
           <div>
             <div className=" font-bold text-lg">New Registers</div>
             <div className=" lg:text-4xl max-sm:text-3xl text-lg font-extrabold font-sans">
@@ -89,7 +89,7 @@ const LandingPage = () => {
             </div>
             <div className="stat-desc">↘︎ 90 (14%)</div>
           </div>
-          <div className=" text-secondary">
+          {/* <div className=" text-secondary"> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -103,7 +103,7 @@ const LandingPage = () => {
                 d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
               ></path>
             </svg>
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </div>
